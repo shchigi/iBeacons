d95a5aa43f20a0b5172bb62fee6a6dae36b1bdd4
Denis Shchigelsky <denis.shchigelsky@gmail.com>
2014-07-16 11:20:21 +0400

    development branch settings

--- iBeacons/settings.py
+++ iBeacons/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/ubuntu/dev/django_projects/iBeacons/iBeacons.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/rakot/PycharmProjects/iBeacons/iBeacons.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
