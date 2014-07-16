56c0c505cc145c34dd2bd75a0de5c0fa039d28f4
Denis Shchigelsky <denis.shchigelsky@gmail.com>
2014-07-16 19:25:25 +0400

    added admin for Person, Event, Category and filled database

--- network/admin.py
+++ network/admin.py
@@ -1,10 +1,13 @@
 __author__ = 'rakot'
 
 from django.contrib import admin
-from models import Object, Beacon, InnerPoint, OuterPoint
+from models import Object, Beacon, InnerPoint, OuterPoint, Person, Category, Event
 
 
 admin.site.register(Object)
 admin.site.register(Beacon)
 admin.site.register(InnerPoint)
 admin.site.register(OuterPoint)
+admin.site.register(Person)
+admin.site.register(Category)
+admin.site.register(Event)
